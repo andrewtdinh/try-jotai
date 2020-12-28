233c670bcf18b8f9d73f372c352f93a480e3e362
Andrew Dinh <andrewd@benchling.com>
2020-12-27 20:05:18 -0800

    Able to fetch pokemon data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { atom, useAtom } from 'jotai';
 
-const filterAtom = atom('')
+const filterAtom = atom('');
+const pokemonAtom = atom([]);
 
 function App() {
   const [filter, filterSet] = useAtom(filterAtom);
+  const [pokemon, pokemonSet] = useAtom(pokemonAtom);
+
+  React.useEffect(() => {
+    fetch('https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823/pokemon.json')
+      .then((resp) => resp.json())
+      .then((data) => pokemonSet(data))
+  }, [])
+
   return (
     <div style={{
       width: 800,
@@ -15,7 +24,7 @@ function App() {
         value={filter}
         onChange={(evt) => filterSet(evt.target.value)}
       ></input>
-      <div>{filter}</div>
+      <div>{JSON.stringify(pokemon)}</div>
     </div>
   );
 }

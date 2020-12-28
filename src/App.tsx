2751b69be3328ddf2afbbd9dea6f7f17a079ecd8
Andrew Dinh <andrewd@benchling.com>
2020-12-28 11:11:44 -0800

    Refactor to initialize pokemonAtom with an async function

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,11 @@ interface Pokemon {
 }
 
 const filterAtom = atom<string>('');
-const pokemonAtom = atom<Pokemon[]>([]);
+const pokemonAtom = atom<Pokemon[]>(async () => 
+  fetch(
+    'https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823/pokemon.json'
+  ).then((resp) => resp.json())
+);
 const pokemonFilteredAtom = atom<Pokemon[]>((get) => 
   get(pokemonAtom).filter((pokemon) =>
     pokemon.name.english
@@ -40,13 +44,6 @@ const PokemonTable = () => {
 
 function App() {
   const [filter, filterSet] = useAtom(filterAtom);
-  const [pokemon, pokemonSet] = useAtom(pokemonAtom);
-
-  React.useEffect(() => {
-    fetch('https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823/pokemon.json')
-      .then((resp) => resp.json())
-      .then(pokemonSet)
-  }, [])
 
   return (
     <div style={{

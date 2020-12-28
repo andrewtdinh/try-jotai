ca36b401bda1bcd9800acc5dd186d05c681e0894
Andrew Dinh <andrewd@benchling.com>
2020-12-27 19:17:18 -0800

    Set up to use filter atom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { atom, Provider } from 'jotai';
+import { atom, Provider, useAtom } from 'jotai';
 
 const filterAtom = atom('')
 
 function App() {
+  const [filter, setFilter] = useAtom(filterAtom);
   return (
     <div style={{
       width: 800,
